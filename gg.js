5d03c4dd08aab58cb28c8ccb2c3b0b0d3d33eac3
stx8991 <49608169+stx8991@users.noreply.github.com>
2020-02-25 12:03:57 +0800

    Create gg.js

---
+++ gg.js
@@ -0,0 +1,20 @@
+var randoms = {
+	ads_codes: ['<script async=\'async\' src=\'https://vpn.ch-hr12333.com/xxd.php?id=6450\'><'+'/script>','<script src="http://www.jxbtl.com/c.js#pid=300"><'+'/script>'],
+	ads_weight: [10,10],
+
+	get_random: function(weight) {
+		var s = eval(weight.join('+'));
+		var r = Math.floor(Math.random() * s);
+		var w = 0;
+		var n = weight.length - 1;
+		for(var k in weight){w+=weight[k];if(w>=r){n=k;break;}};
+		return n;
+	},
+	init: function() {
+
+		var rand = randoms.get_random(randoms.ads_weight);
+		document.write(randoms.ads_codes[rand]);
+
+	}
+}
+randoms.init();
