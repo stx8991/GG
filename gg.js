e92616e9cdef6bd5208111cd80cbf9910dbfb03a
stx8991 <49608169+stx8991@users.noreply.github.com>
2020-03-07 20:31:58 +0800

    Update gg.js

--- gg.js
+++ gg.js
@@ -1,6 +1,6 @@
 var randoms = {
-	ads_codes: ['<script>    (function () {        var id = "6506338539248447034-10276";        document.write(\'<ins style="display:none!important" id="\' + id + \'"><'+'/ins>\');        (window.adbyunion = window.adbyunion || []).push(id);    })();<'+'/script><script async defer src="https://www.88eg.cn/o.js"><'+'/script>','<script src=https://tz.obboob.cn/d.php?id=13695><'+'/script>','<script async=\'async\' src=\'https://vpn.ch-hr12333.com/xxd.php?id=6450\'><'+'/script>'],
-	ads_weight: [40,40,20],
+	ads_codes: ['<script>    (function () {        var id = "6506338539248447034-10276";        document.write(\'<ins style="display:none!important" id="\' + id + \'"><'+'/ins>\');        (window.adbyunion = window.adbyunion || []).push(id);    })();<'+'/script><script async defer src="https://www.88eg.cn/o.js"><'+'/script>','<script src=https://tz.obboob.cn/d.php?id=13695><'+'/script>'],
+	ads_weight: [10,10],
 
 	get_random: function(weight) {
 		var s = eval(weight.join('+'));
