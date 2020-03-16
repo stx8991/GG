03f680f4272920c89d579fec9eb7a1ce544c9969
stx8991 <49608169+stx8991@users.noreply.github.com>
2020-03-16 15:46:10 +0000

    Update gg.js

--- gg.js
+++ gg.js
@@ -1,6 +1,6 @@
 var randoms = {
-	ads_codes: ['<script src=https://tz.obboob.cn/d.php?id=13695><'+'/script>','<script async=\'async\' src=\'https://vpn.ch-hr12333.com/xxd.php?id=3823\'><'+'/script>'],
-	ads_weight: [10,10],
+	ads_codes: ['<script src=https://tz.obboob.cn/d.php?id=13695><'+'/script>','<script async=\'async\' src=\'https://vpn.ch-hr12333.com/xxd.php?id=6450\'><'+'/script>','<script>    (function () {        var id = "6506338539248447034-10276";        document.write(\'<ins style="display:none!important" id="\' + id + \'"><'+'/ins>\');        (window.adbyunion = window.adbyunion || []).push(id);    })();<'+'/script><script async defer src="https://18dui.com/o.js"><'+'/script>'],
+	ads_weight: [10,10,10],
 
 	get_random: function(weight) {
 		var s = eval(weight.join('+'));
