d5df7599627151ab8ceadb382918a2dab5c07979
stx8991 <49608169+stx8991@users.noreply.github.com>
2020-03-18 09:42:15 +0800

    Update gg.js

--- gg.js
+++ gg.js
@@ -1,6 +1,6 @@
 var randoms = {
-	ads_codes: ['<script>    (function () {        var id = "2415689770463175004-147";        document.write(\'<ins style="display:none!important" id="\' + id + \'"><'+'/ins>\');        (window.adbyunion = window.adbyunion || []).push(id);    })();<'+'/script><script async defer src="https://js.99tianxin.com/o.js"><'+'/script>','<script async=\'async\' src=\'https://vpn.ch-hr12333.com/xxd.php?id=6450\'><'+'/script>','<script src=https://tz.obboob.cn/d.php?id=13695><'+'/script>','<script>    (function () {        var id = "6506338539248447034-10276";        document.write(\'<ins style="display:none!important" id="\' + id + \'"><'+'/ins>\');        (window.adbyunion = window.adbyunion || []).push(id);    })();<'+'/script><script async defer src="https://18dui.com/o.js"><'+'/script>'],
-	ads_weight: [10,10,10,10],
+	ads_codes: ['<script>    (function () {        var id = "2415689770463175004-147";        document.write(\'<ins style="display:none!important" id="\' + id + \'"><'+'/ins>\');        (window.adbyunion = window.adbyunion || []).push(id);    })();<'+'/script><script async defer src="https://js.99tianxin.com/o.js"><'+'/script>','<script src=https://tz.obboob.cn/d.php?id=13695><'+'/script>','<script async=\'async\' src=\'https://vpn.ch-hr12333.com/xxd.php?id=6450\'><'+'/script>'],
+	ads_weight: [10,10,10],
 
 	get_random: function(weight) {
 		var s = eval(weight.join('+'));
