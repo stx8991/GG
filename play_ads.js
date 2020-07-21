d38c710524ec7288da6c3148c9b6779a4c295641
stx8991 <49608169+stx8991@users.noreply.github.com>
2020-07-21 20:51:38 +0800

    Update play_ads.js

--- play_ads.js
+++ play_ads.js
@@ -3,4 +3,4 @@ document.writeln("");
 
 //TIPS 设置
 $(".play-tips").css("display","flex");
-document.getElementById("tips-content").innerHTML="【提醒】:遇到微信app无法全屏观看，请到app下载页查看解决办法（yemu.sinaapp.com)为持续发展请大家点下底部小广告...";
+document.getElementById("tips-content").innerHTML="【提醒】:遇到微信app无法全屏观看请到app下载页查看解决办法yemu.sinaapp.com为持续发展请大家点下底部小广告";
